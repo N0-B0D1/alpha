8b9c200bd277f60466327b20dd398808fa1bc4c4
Jason Wendlandt <jason.wendlandt@cypherpath.com>
2015-01-16 16:15:37 -0800

    Enhanced logger to support variadic arguement list
    Is now much more user friendly, and robust.
    Will also write out each line, then close.  So that in the case of a segfault or other issue that causes the program to exit incorrectly, the log file will still be complete up to that point in execution.

--- AlphaEngine/Include/Toolbox/Logger.h
+++ AlphaEngine/Include/Toolbox/Logger.h
@@ -21,41 +21,65 @@ namespace alpha
     {
     public:
         explicit Logger(const std::string & path)
-            : m_lineNumber(0)
+            : m_path(path)
+            , m_lineNumber(0)
         {
-            m_outStream.open(path.c_str(), std::ios_base::binary | std::ios_base::out);
+            //m_outStream.open(path.c_str(), std::ios_base::binary | std::ios_base::out);
+            // remove the file if it already exists, so we can start a fresh log
+            remove(path.c_str());
         }
         virtual ~Logger()
         {
-            m_outStream.close();
+            //m_outStream.close();
         }
 
-        template<Severity severity>
-        void print(const std::string & msg)
+        template<Severity severity, typename...Args>
+        void print( Args...args )
         {
+            // take write lock, and open file for appending only
             m_writeMutex.lock();
+            m_outStream.open(m_path.c_str(), std::ios_base::app | std::ios_base::out);
 
-            m_outStream << this->GetLineNumber();
+            //if (m_outStream.open)
+            {                
+                m_outStream << this->GetLineNumber();
 
-            switch (severity)
-            {
-                case Severity::DEBUG:
-                    m_outStream << "<DEBUG>   : ";
-                    break;
-                case Severity::WARNING:
-                    m_outStream << "<WARNING> : ";
-                    break;
-                case Severity::ERROR:
-                    m_outStream << "<ERROR>   : ";
-                    break;
+                switch (severity)
+                {
+                    case Severity::DEBUG:
+                        m_outStream << "[  DEBUG  ] : ";
+                        break;
+                    case Severity::WARNING:
+                        m_outStream << "[*WARNING*] : ";
+                        break;
+                    case Severity::ERROR:
+                        m_outStream << "[!!ERROR!!] : ";
+                        break;
+                }
+
+                //m_outStream << msg << std::endl;
+                print_impl(args...);
             }
-
-            m_outStream << msg << std::endl;
+            m_outStream.close();
 
             m_writeMutex.unlock();
         }
 
     private:
+        void print_impl()
+        {
+            //policy->write( get_logline_header() + log_stream.str() );
+            //m_outStream << this.GetLineNumber();
+            m_outStream << std::endl;
+        }
+         
+        template<typename First, typename...Rest>
+        void print_impl(First param1, Rest...rest)
+        {
+            m_outStream << param1;
+            print_impl(rest...);    
+        }
+
         std::string GetLineNumber()
         {
             std::stringstream number;
@@ -67,6 +91,8 @@ namespace alpha
             
             return number.str();
         }
+
+        const std::string m_path;
 
         unsigned m_lineNumber;
         std::mutex m_writeMutex;
