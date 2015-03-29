5465105d225cb1d8e8aefa120a5fb19766d3030c
Jason R. Wendlandt <BakaOtaku@gmail.com>
2015-03-29 12:18:45 -0700

    Fixed Vertex rendering in OpenGL
    Converted tes lua entity to use mesh component.

--- AlphaEngine/Source/Unix/Graphics/GraphicsRenderer.cpp
+++ AlphaEngine/Source/Unix/Graphics/GraphicsRenderer.cpp
@@ -151,15 +151,19 @@ namespace alpha
                 
                     // copy vertices into vertex buffer
                     glBindBuffer(GL_ARRAY_BUFFER, renderable->m_vertexBuffer);
-                    glBufferData(GL_ARRAY_BUFFER, renderable->vertices.size() * sizeof(GLfloat), renderable->vertices.data(), GL_STATIC_DRAW);
+                    glBufferData(GL_ARRAY_BUFFER, renderable->vertices.size() * sizeof(Vertex), renderable->vertices.data(), GL_STATIC_DRAW);
 
                     // set indicies into element buffer
                     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, renderable->m_elementBuffer);
                     glBufferData(GL_ELEMENT_ARRAY_BUFFER, renderable->indices.size() * sizeof(GLuint), renderable->indices.data(), GL_STATIC_DRAW);
 
                     // set vertex attribute pointers
-                    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
                     glEnableVertexAttribArray(0);
+                    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)0);
+
+                    // Vertex Normals
+                    glEnableVertexAttribArray(1);
+                    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, normal));
                 
                 glBindVertexArray(0);
             }
