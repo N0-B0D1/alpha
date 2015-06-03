f68e5c4ddf6480290b55b39b351bbbc2c4370389
Jason R. Wendlandt <BakaOtaku@gmail.com>
2015-06-02 21:40:19 -0700

    Added missing content and component type
    Ade light component class and material content

---
+++ AlphaEngine/Source/Entities/LightComponent.cpp
@@ -0,0 +1,71 @@
+/**
+Copyright 2014-2015 Jason R. Wendlandt
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "Entities/LightComponent.h"
+#include "Scripting/LuaVar.h"
+#include "Graphics/Material.h"
+
+namespace alpha
+{
+    const std::string LightComponent::sk_name = "light";
+
+    LightComponent::~LightComponent() { }
+
+    //! Provides logic for how to initialize a transform component from Lua script data
+    void LightComponent::VInitialize(std::shared_ptr<LuaVar> var)
+    {
+        std::shared_ptr<LuaTable> table = std::dynamic_pointer_cast<LuaTable>(var);
+
+        // get direct light intensity
+        std::shared_ptr<LuaVar> intensity_var = table->Get("intensity");
+        if (intensity_var->GetVarType() == VT_STATIC)
+        {
+            auto intensity = std::dynamic_pointer_cast<LuaStatic<double>>(intensity_var);
+            m_fIntensity = static_cast<float>(intensity->GetValue());
+        }
+
+        // get indirect ambient intensity
+        std::shared_ptr<LuaVar> ambient_var = table->Get("ambient_intensity");
+        if (ambient_var->GetVarType() == VT_STATIC)
+        {
+            auto ambient = std::dynamic_pointer_cast<LuaStatic<double>>(ambient_var);
+            m_fAmbientIntensity = static_cast<float>(ambient->GetValue());
+        }
+
+        // init base scene component
+        SceneComponent::VInitialize(var);
+    }
+
+    bool LightComponent::VUpdate(float /*fCurrentTime*/, float /*fElapsedTime*/)
+    {
+        return true;
+    }
+
+    std::string LightComponent::VGetName() const
+    {
+        return LightComponent::sk_name;
+    }
+
+    float LightComponent::GetIntensity() const
+    {
+        return m_fIntensity;
+    }
+
+    float LightComponent::GetAmbientIntensity() const
+    {
+        return m_fAmbientIntensity;
+    }
+}
---
+++ Demos/Content/Materials/bronze.lua
@@ -0,0 +1,21 @@
+-- Copyright 2014-2015 Jason R. Wendlandt
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+-- Materials data table
+material = {
+	color = { r = 1.0, g = 0.5, b = 0.31, a = 1.0 },
+	roughness = 0.5,
+	metallic = 0.0,
+	specular = 0.5,
+}
