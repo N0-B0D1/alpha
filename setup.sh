154d1381243a0e34a4e7f6b5caf013fe7be52ca6
Jason R. Wendlandt <BakaOtaku@gmail.com>
2015-10-27 07:50:03 -0700

    Use our own build of SDL2
    Setup script pulls SDL2 repo, manages sources that are needed and provides our own custom cmakelists.txt to build it.

--- setup.sh
+++ setup.sh
@@ -1,14 +1,39 @@
 #!/bin/sh
 
+SDL_DIRECTORY=./3rdParty/SDL2
+
 # initialize and update submodules
 echo "Init and Update GIT submodules ..."
 git submodule init
 git submodule update
 
-SDL_DIRECTORY=./3rdParty/SDL2
-
 # Clone sdl2 repository into 3rdParty directory
-if [ ! -d "$SDL_DIRECTORY" ]; then
+if [ ! -d "$SDL_DIRECTORY/repo" ]; then
     echo "Cloning SDL2 ..."
-    hg clone --updaterev release-2.0.3 http://hg.libsdl.org/SDL $SDL_DIRECTORY
+    hg clone --updaterev release-2.0.3 http://hg.libsdl.org/SDL $SDL_DIRECTORY/repo
+fi
+
+# copy source and include files into base sdl2 directory
+if [ -d "$SDL_DIRECTORY/repo" ]; then
+
+    if [ ! -d "$SDL_DIRECTORY/include" ]; then
+        echo "Copying header files ..."
+        cp -rv $SDL_DIRECTORY/repo/include $SDL_DIRECTORY/include
+    fi
+
+    if [ ! -d "$SDL_DIRECTORY/src" ]; then
+        echo "Copying source files ..."
+        cp -rv $SDL_DIRECTORY/repo/src $SDL_DIRECTORY/src
+    fi
+
+    if [ ! -d "$SDL_DIRECTORY/cmake" ]; then
+        echo "Copying cmake files ..."
+        cp -rv $SDL_DIRECTORY/repo/cmake $SDL_DIRECTORY/cmake
+    fi
+
+    echo "Copying cmake files ..."
+    cp -v $SDL_DIRECTORY/repo/sdl2-config.in $SDL_DIRECTORY
+    cp -v $SDL_DIRECTORY/repo/sdl2.pc.in $SDL_DIRECTORY
+    cp -v $SDL_DIRECTORY/repo/SDL2.spec.in $SDL_DIRECTORY
+    cp -v $SDL_DIRECTORY/repo/sdl2.m4 $SDL_DIRECTORY
 fi
